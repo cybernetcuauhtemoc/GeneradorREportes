bc086ae948c449991988352afc5893990d2fc2e8
cybernetcuauhtemoc <cybernetcuauhtemoc@gmail.com>
2026-01-29 11:21:21 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,36 @@
-const CACHE_NAME = 'reset-v9';
+const CACHE_NAME = 'reporte-v10';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icono.png',
+  './jspdf.umd.min.js'
+];
 
-self.addEventListener('install', (event) => {
-    self.skipWaiting(); // Obliga al nuevo SW a tomar el control de inmediato
+// Al instalar, guardamos lo básico
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => caches.delete(cacheName))
-            );
-        })
-    );
+// Al activar, borramos caches viejos
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k)));
+    })
+  );
 });
 
+// LÓGICA OFFLINE: Intenta red, si falla, usa el cache
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
-    );
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
